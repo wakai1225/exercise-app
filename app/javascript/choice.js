ae0e10e044a777da4cbd81451b1be67cdacf5b7b
stmaho <121847812+stmaho@users.noreply.github.com>
2023-04-29 15:09:40 +0900

    取得したURLのサムネイル表示

--- app/javascript/choice.js
+++ app/javascript/choice.js
@@ -5,13 +5,17 @@
       PicUp.addEventListener('click', function() {
         event.preventDefault();
         const linkUrl = this.href; // リンクのURLを取得する
-        console.log(linkUrl); // コンソールに表示する
+        const id = linkUrl.split("shorts")[1];
+        const HtmlUrl = "https://youtube.com/embed/" + id;
+        document.getElementById('url_display').src = HtmlUrl ;
+        
       });
       PicUp.addEventListener('mouseover', function(){
         PicUp.setAttribute("style", "color: red;")
-    });
-    PicUp.addEventListener('mouseout', function(){
-      PicUp.setAttribute("style", "color: black;")
-    });
+      });
+      PicUp.addEventListener('mouseout', function(){
+        PicUp.setAttribute("style", "color: black;")
+      });
   });
+  const submit = document.getElementById("button");
   });
