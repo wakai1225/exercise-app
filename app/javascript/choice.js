e2c3cbada63bebe7695596deed796c6d7f2028e8
stmaho <121847812+stmaho@users.noreply.github.com>
2023-04-30 17:13:14 +0900

    url テキストの表示

--- app/javascript/choice.js
+++ app/javascript/choice.js
@@ -2,21 +2,34 @@
   window.addEventListener('load', function() {
     const  PicUp = document.querySelectorAll ('.exe');
     PicUp.forEach(function(PicUp) {
-      PicUp.addEventListener('mouseover', function(){
+      PicUp.addEventListener('mouseover', function(e){
         PicUp.setAttribute("style", "color: red;")
+        e.preventDefault();
+        const linkUrl = this.href; // リンクのURLを取得する
+        const id = linkUrl.split("shorts")[1]; //リンクidの取得
+        const HtmlUrl = "https://youtube.com/embed/" + id; //HTML適応するURLに変換
+        document.getElementById('url_display').src = HtmlUrl ; //HTMLにサムネイル表示
       });
+
       PicUp.addEventListener('mouseout', function(){
         PicUp.setAttribute("style", "color: black;")
       });
+
       PicUp.addEventListener('click', function(e) {
         e.preventDefault();
         const linkUrl = this.href; // リンクのURLを取得する
-        const id = linkUrl.split("shorts")[1];
-        const HtmlUrl = "https://youtube.com/embed/" + id;
-        document.getElementById('url_display').src = HtmlUrl ;
-        document.getElementById("url_text").value = HtmlUrl;
-
+        const id = linkUrl.split("shorts")[1]; //リンクidの取得
+        const HtmlUrl = "https://youtube.com/embed/" + id; //HTML適応するURLに変換
+        document.getElementById('url_display').src = HtmlUrl ; //HTMLにサムネイル表示
+        document.getElementById("url_text").value = HtmlUrl; //text入力
       });
     });
 
+
+    function hoge() {
+      document.getElementById("url_text").value='';
+    }
+    
+    document.getElementById("btn").addEventListener('click', hoge, false);
+    
   });
