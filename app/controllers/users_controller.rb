57bbe207a08180dd22d1e1e65859d8156d6c4e94
stmaho <121847812+stmaho@users.noreply.github.com>
2023-05-03 21:05:42 +0900

    show.html.erbの編集
    ユーザーが登録したexerciseをサムネイルで表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: :show
   def show
     @user = User.find(params[:id])
-    @urls= exercises_url
+    @exercises = @user.exercises
   end
-
 end
